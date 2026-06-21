da23197c8628a11cdda6595e898bd634f7699872
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-06-21 19:41:01 +0100

    Create power-ballad.js with genre notes
    Added a new file with notes on 80s power ballads.

---
+++ strudel/genre-exercises/power-ballad.js
@@ -0,0 +1,19 @@
+/*
+80s power ballads
+    70-90 bpm (or 130+ halftime)
+    Belting vocals
+    Synth strings
+    Piano
+    Halftimey reverby drums that often come in later
+    Reverb on everything
+    Guitar solo?
+    Key changes!
+    Constant emotional escalation
+    Examples: Beverley Craven - Promise Me (of course!!) https://www.youtube.com/watch?v=EmTBKbyaEjU
+    Bryan Adams - (Everything I Do) I Do It For You
+    Bonnie Tyler - Total Eclipse of the Heart https://www.youtube.com/watch?v=XMmXCyrV_WQ
+    George Michael - Careless Whisper (here the saxophone does the belting…) https://www.youtube.com/watch?v=izGwDsrQ1eQ
+*/
+
+
+
