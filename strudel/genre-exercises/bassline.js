dacd8ddaebdfe4c779d7d96820a3988a0e1dd373
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-06-21 19:40:21 +0100

    Add bassline definition and examples to bassline.js
    Added comments detailing bassline characteristics and examples.

---
+++ strudel/genre-exercises/bassline.js
@@ -0,0 +1,13 @@
+/*
+Bassline:
+    135-142 bpm
+    Wub bass - envelope filtered/LFO
+    Often multiple intricate basslines
+    Bass offbeat from drums
+    Often a half speed section
+    UK garage drum pattern
+    Sped up female RnB vocals
+    Examples: TS7 -
+    Come on Over (feat. Bianca Gerald) https://www.youtube.com/watch?v=Hp6LmRR6X5E
+    Thorpey - I Can Love U https://offmenutrecords.bandcamp.com/track/i-can-love-u
+*/
