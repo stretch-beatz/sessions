713bca96b3efa64d11005c7d64dd83c6ca7e091f
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-06-21 19:56:41 +0100

    Starting to work out structure
    Removed commented-out Rockstar program and simplified verse definitions.

--- strudel/genre-exercises/power-ballad.js
+++ strudel/genre-exercises/power-ballad.js
@@ -20,21 +20,34 @@ await init()
     George Michael - Careless Whisper (here the saxophone does the belting…) https://www.youtube.com/watch?v=izGwDsrQ1eQ
 */
 
-/*
-// Run a Rockstar program
-const prog = await rockstar_pro`
+// 70-90 bpm (or 130+ halftime)
+// Belting vocals
+// Synth strings
+// Piano
+// Halftimey reverby drums that often come in later
+// Reverb on everything
+// Guitar solo?
+// Key changes!
+// Constant emotional escalation
+
+
+// Rockstar for verse 1
+const verse1_prog = await rockstar_pro`
   Papa was a rolling stone
   (Wherever he laid his hat)
-  (was his home)
-  (And when he died)
-  (All he left us was alone)
-  Say Papa
-  Build Papa up up up
-  Scream Papa
-  Mama said knock you out
-  Shout Mama
-`
-*/
+  Say Papa`
+
+// Rockstar for chorus
+const verse1_prog = await rockstar_pro`
+  Papa was a rolling stone
+  (Wherever he laid his hat)
+  Say Papa`
+
+// Rockstar for verse 2
+const verse2_prog = await rockstar_pro`
+  Papa was a rolling stone
+  (Wherever he laid his hat)
+  Say Papa`
 
 // $_: n(base(prog.output).slow(2)).scale(my_scale). s("supersaw").gain(1.5)
 // samples('shabda/phonemes/en-GB/m:'+prog.speech.join(',')+'?force=0&overrides=papa:P_AA1_P_A')
