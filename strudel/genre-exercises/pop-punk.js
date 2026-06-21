4678b2f79b49503cb1fe38f128f2cda5e21b64a5
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-06-21 19:41:35 +0100

    Add pop punk genre description and examples
    Added comments detailing characteristics and examples of pop punk music.

---
+++ strudel/genre-exercises/pop-punk.js
@@ -0,0 +1,12 @@
+/*
+Pop punk:
+    Wide range of tempo but generally fast at 150-190bpm
+    Between Bad Religion and McFly
+    Distorted guitars
+    Shouty nasal vocals
+    Rock drum beat
+    ‘Anti surburbia themes’
+    Examples: The Offspring - The Kids Aren’t Alright https://www.youtube.com/watch?v=7iNbnineUCI
+    All American Rejects - Gives You Hell https://www.youtube.com/watch?v=uxUATkpMQ8A
+    Andrew W.K. - Party Hard https://www.youtube.com/watch?v=WccfbPQNMbg
+*/
